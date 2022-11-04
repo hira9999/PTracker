32cc4e70747c7ab7f0ea04e66b39d97ee1d14127
hira9999 <iedddd@naver.com>
2022-11-04 21:38:01 +0900

    cover components to queryClientProvider

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,18 @@
 import DashboardLayout from '../components/dashboard/Dashboard';
 import { TrackerProvider } from '../context/traker/trackerContext';
 import privateRoute from '../container/privateRoute';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+const queryClient = new QueryClient();
 function Dashboard() {
   return (
-    <TrackerProvider>
-      <DashboardLayout />
-    </TrackerProvider>
+    <QueryClientProvider client={queryClient}>
+      <TrackerProvider>
+        <DashboardLayout />
+      </TrackerProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
