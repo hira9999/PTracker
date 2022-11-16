72d1fb5e12b37e8134330a76d8819f6a598426fe
hira9999 <iedddd@naver.com>
2022-11-16 20:54:41 +0900

    init

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+const GET_TRACKERS_STALETIME = 60 * 1000;
+
+const PRODUCT_INFO_SELECTOR =
+  '#__next > div > div.style_container__D_mqP > div.style_inner__ZMO5R > div.style_content_wrap__78pql > div.style_content__v25xx > div > div.summary_info_area__NP6l5 > div.lowestPrice_price_area__VDBfj';
+
+const LOWEST_PRICE_BASE_URL = 'https://search.shopping.naver.com/catalog/';
+
+export { GET_TRACKERS_STALETIME, PRODUCT_INFO_SELECTOR, LOWEST_PRICE_BASE_URL };
